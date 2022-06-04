0898cc7235d52387661902922076d23071f1d54a
John Bista <bistajohn2058@gmail.com>
2022-06-04 21:38:12 +0545

    day5

--- blog/settings.py
+++ blog/settings.py
@@ -76,10 +76,23 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'DB_blog' , 
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'J0hnbist@',
+
+        'HOST': 'localhost',
+
+        'PORT': '',
+
     }
+
 }
 
 
